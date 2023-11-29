45612ea509e269b4e61688d349794ce0d0be063e
milkfist <alyonazzz12@mail.ru>
2023-11-29 16:58:34 +0300

    added Query Params in ApiService

--- app/src/main/java/com/example/moviesjava/ApiService.java
+++ app/src/main/java/com/example/moviesjava/ApiService.java
@@ -2,9 +2,10 @@ package com.example.moviesjava;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 interface ApiService {
 
     @GET("movie?token=K5416T0-EJ04XMY-QCB0F37-BXZMYWW")
-    Single<ServerResponse> loadMovies();
+    Single<ServerResponse> loadMovies(@Query("page") int page);
 }
--- app/src/main/java/com/example/moviesjava/MainActivity.java
+++ app/src/main/java/com/example/moviesjava/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.moviesjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.Disposable;
@@ -12,24 +16,26 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity1";
+
+    MainViewModel mainViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Disposable disposable = ApiFactory.apiService.loadMovies()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<ServerResponse>() {
-                    @Override
-                    public void accept(ServerResponse serverResponse) throws Throwable {
-                        Log.d("MainActivity1", serverResponse.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.d("MainActivity1", throwable.toString());
-                    }
-                });
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        mainViewModel.getMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> movies) {
+//                Log.d(TAG, movies.toString());
+                Log.d(TAG, movies.get(0).getName().toString());
+            }
+        });
+//        if (mainViewModel.getMovies() == null)
+//            Log.d(TAG, "movies.get(0).getName().toString()");
+        mainViewModel.loadMovies();
     }
+
 }
